856cf752da45dfdc862b12f90aed17f838d388a9
Issac Abraham <issaca32@gmail.com>
2021-01-30 14:55:51 +0000

    Draws 9x9 Grid sepeating grids horizontaly and Verticaly

--- grid.py
+++ grid.py
@@ -11,7 +11,7 @@ class GameGrid:
             GameGrid.game_grid.append(row)
 
     def print_grid(self):
-        for row in GameGrid.game_grid:
+        for row_index, row in enumerate(GameGrid.game_grid):
             for column_index, column in enumerate(row):
                 if (column_index + 1) % 9 != 0:
                     if (column_index+1) != 9 and (column_index + 1) % 3 == 0:
@@ -19,6 +19,8 @@ class GameGrid:
                     else:
                         print(column, end="")
                 else:
-                    print(column)
+                    print(column, end="\n")
+                    if (row_index + 1) % 3 == 0:
+                        print('-'*20)
 
 
