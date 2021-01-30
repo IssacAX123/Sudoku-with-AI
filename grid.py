43246934963b4115688ae55b68a8574aa8806fa0
Issac Abraham <issaca32@gmail.com>
2021-01-30 15:06:48 +0000

    Adds number indexes for cells

--- grid.py
+++ grid.py
@@ -1,5 +1,6 @@
 class GameGrid:
     game_grid = []
+
     def __init__(self, difficulty):
         self.difficulty = difficulty
 
@@ -11,16 +12,19 @@ class GameGrid:
             GameGrid.game_grid.append(row)
 
     def print_grid(self):
+        print('  1 2 3  4 5 6  7 8 9')
         for row_index, row in enumerate(GameGrid.game_grid):
             for column_index, column in enumerate(row):
                 if (column_index + 1) % 9 != 0:
-                    if (column_index+1) != 9 and (column_index + 1) % 3 == 0:
+                    if (column_index + 1) != 9 and (column_index + 1) % 3 == 0:
                         print(column, end="-")
                     else:
-                        print(column, end="")
+                        if (column_index + 1) % 9 == 1:
+                            print(row_index + 1, end=" ")
+                            print(column, end="")
+                        else:
+                            print(column, end="")
                 else:
                     print(column, end="\n")
                     if (row_index + 1) % 3 == 0:
-                        print('-'*20)
-
-
+                        print(' ', '-' * 20)
